0b8fc75957ad6bf2292866276d7e565297b5ef48
Yura Dubinin <yuradubinin100@gmail.com>
2022-06-29 00:01:29 +0700

    Added module 'tabs.js'
    * 'tabs.js' provides the functionality of tabs, used in two places
    
    * Added 'additional_classes.css for simple animation of tabs'

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,44 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent() {
+        content.forEach((item) => {
+            item.classList.add("hide");
+            item.classList.remove("show", "fade");
+        });
+
+        tab.forEach((item) => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i = 0) {
+        content[i].classList.add("show", "fade");
+        content[i].classList.remove("hide");
+        tab[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener("click", (event) => {
+        const target = event.target;
+
+        if (
+            target &&
+            (target.classList.contains(tabSelector.replace(/\./, "")) ||
+                target.parentNode.classList.contains(tabSelector.replace(/\./, "")))
+        ) {
+            tab.forEach((item, i) => {
+                if (target === item || target.parentNode === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+};
+
+export default tabs;
