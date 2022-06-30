3d84352614cf3f8f3e4b9eba076d1ab64454e113
Yura Dubinin <yuradubinin100@gmail.com>
2022-06-30 13:52:25 +0700

    Added module temer.js
    * Module timer.js provides a timer on a page
    
    * Also added the necessary checks for the timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 window.addEventListener("DOMContentLoaded", () => {
     "use strict";
@@ -10,10 +11,14 @@ window.addEventListener("DOMContentLoaded", () => {
     // Variable for the modal window, collecting information about user`s choice in the calculator form
     let modalState = {};
 
+    // Timer deadline
+    let deadline = "2022-12-31";
+
     changeModalState(modalState);
     modals();
     tabs(".glazing_slider ", ".glazing_block", ".glazing_content", "active");
     tabs(".decoration_slider", ".no_click", ".decoration_content > div > div", "after_click");
     tabs(".balcon_icons", ".balcon_icons_img", ".big_img > img", "do_image_more", "inline-block");
     forms(modalState);
+    timer(".container1", dead);
 });
